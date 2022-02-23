f84806bca42d10816e96c26ef5b6d44048b668be
Kevan Williams <kevanwilliams@Kevans-MacBook-Air.local>
2022-02-22 19:26:04 -0500

    afwuew

---
+++ build/precache-manifest.c32e7cb1c5721398702d6429dc6120b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4ead65c93fddb54373c6539db66cb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9222361105c8728b4243",
+    "url": "/static/css/main.5263404e.chunk.css"
+  },
+  {
+    "revision": "2bc9d19d9da8aa5e12b8",
+    "url": "/static/js/2.fb7ac2df.chunk.js"
+  },
+  {
+    "revision": "9222361105c8728b4243",
+    "url": "/static/js/main.cd81b352.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
